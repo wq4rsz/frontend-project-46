06992fd5f0bcb80d6feaba604c3b4b713a85d03b
wq4rsz <dfirsova648@gmail.com>
2023-09-11 15:52:54 +0300

    update bin/gendiff.js package.json add Makefile

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+ 
+const program = new Command();
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .option('-V, --version', 'output the version number')
+    .option('-h, --help', 'display help for command')
+
+program.parse();
