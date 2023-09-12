8696a39a82e0acb711074bb4da3b5a3c75998fa3
wq4rsz <dfirsova648@gmail.com>
2023-09-12 19:33:17 +0300

    fixed all

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
 
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'display help for command')
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format', 'stylish')
 
 program.parse();
